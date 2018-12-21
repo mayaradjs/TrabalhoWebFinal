4bda5a9a80a5c3712061d1f2e6b2053c4183d6d0
Mayara Silva <maydkt@gmail.com>
2018-12-21 08:42:39 -0300

    chart

--- scripts/main.js
+++ scripts/main.js
@@ -95,7 +95,9 @@ function renderProposicoes(props) {
   for (var i = 0; i < props.length; i++) {
     var tr = document.createElement('tr');
     var td = document.createElement('td');
-    td.innerHTML = props[i].ementa;
+    td.innerHTML = "<b><a href='Detalhe.html?id=" + props[i].id + "' target='_blank'>" +
+      props[i].siglaTipo + props[i].numero + "</a></b> (" + props[i].ano + ") <br>" +
+      props[i].ementa + "<br>";
     td.appendChild(document.createTextNode('\u0020'))
     tr.appendChild(td)
     tbdy.appendChild(tr);
@@ -103,42 +105,3 @@ function renderProposicoes(props) {
   tbl.appendChild(tbdy);
   body.appendChild(tbl);
 }
-
-//Exemplo de gráfico abaixo (pode remover)
-var ctx = document.getElementById("myChart").getContext('2d');
-var myChart = new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      backgroundColor: [
-        'rgba(255, 99, 132, 0.2)',
-        'rgba(54, 162, 235, 0.2)',
-        'rgba(255, 206, 86, 0.2)',
-        'rgba(75, 192, 192, 0.2)',
-        'rgba(153, 102, 255, 0.2)',
-        'rgba(255, 159, 64, 0.2)'
-      ],
-      borderColor: [
-        'rgba(255,99,132,1)',
-        'rgba(54, 162, 235, 1)',
-        'rgba(255, 206, 86, 1)',
-        'rgba(75, 192, 192, 1)',
-        'rgba(153, 102, 255, 1)',
-        'rgba(255, 159, 64, 1)'
-      ],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true
-        }
-      }]
-    }
-  }
-});
